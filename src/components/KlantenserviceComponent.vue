413fee23c92ff06a0279f0133e0e804bde620a52
RubenJamartPXL <ruben.jamart@student.pxl.be>
2024-05-26 10:37:00 +0200

    Fixed images klantenservice netlify

--- src/components/KlantenserviceComponent.vue
+++ src/components/KlantenserviceComponent.vue
@@ -18,8 +18,8 @@
     data() {
       return {
         images: [
-          { src: '@/assets/images/Aanbieding1.jpg', info: 'Information for Image 1' },
-          { src: '@/assets/images/Aanbieding1.jpg', info: 'Information for Image 2' },
+          { src: '/assets/images/Aanbieding1.jpg', info: 'Information for Image 1' },
+          { src: '/assets/images/Aanbieding1.jpg', info: 'Information for Image 2' },
           { src: 'image3.jpg', info: 'Information for Image 3' },
           { src: 'image4.jpg', info: 'Information for Image 4' },
           { src: 'image5.jpg', info: 'Information for Image 5' },
